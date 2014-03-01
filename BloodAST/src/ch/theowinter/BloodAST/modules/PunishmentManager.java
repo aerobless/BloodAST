cf0cfa2a668152f0b96dded77b4ab5e88c49170c
aerobless <theo.winter@gmail.com>
2014-03-01 13:54:51 +0100

    First implementation of the warn command. Currently only warns the player doesn't get logged yet. This will be my next task.

---
+++ BloodAST/src/ch/theowinter/BloodAST/modules/PunishmentManager.java
@@ -0,0 +1,49 @@
+package ch.theowinter.BloodAST.modules;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import ch.theowinter.BloodAST.MainBloodAST;
+
+public class PunishmentManager implements CommandExecutor {
+	private MainBloodAST main;
+
+	public PunishmentManager(MainBloodAST mainClass) {
+		super();
+		main = mainClass;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		boolean success = false;
+    	if(cmd.getName().equalsIgnoreCase("warn")){
+    		if(args[0].equals(sender.getName())){
+        		sender.sendMessage("Sorry.. but you can't warn yourself dude.");
+    		}
+    		else{
+        		if(warnPlayer(args[0], args[1])){
+        			sender.sendMessage("Successfully warned "+args[0]);
+        		}
+        		else{
+        			sender.sendMessage("Unable to warn "+args[0]+". Are you sure you spelled his name right?");
+        		}
+    		}
+    		success = true;
+    	}
+		return success;
+	}
+	
+	public boolean warnPlayer(String playername, String warnMessage){
+		boolean success = false;
+		Player player = main.getServer().getPlayer(playername);
+		if(player != null){
+			player.sendMessage(ChatColor.RED+"DISCIPLINARY WARNING: "+warnMessage);
+			success = true;
+		}
+		return success;
+	}
+
+}
